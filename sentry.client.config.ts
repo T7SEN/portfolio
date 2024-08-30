511574db2ed7ee83364eb1429b1331d1f4ec1163
T7SEN. <A.hitelare2@gmail.com>
2024-08-30 10:06:56 +0300

    Added Sentry

---
+++ sentry.client.config.ts
@@ -0,0 +1,37 @@
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  // You can find your project ID in your Sentry dashboard's project settings.
+  dsn: "https://2a36ef9e89afed17ba12a1c52c14c0c1@o1032877.ingest.us.sentry.io/4507864900239360",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // You can adjust this value in production, or use tracesSampler for greater control
+  replaysOnErrorSampleRate: 1.0,
+
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+
+  // You can remove this option if you're not planning to use the Sentry Session Replay feature:
+  integrations: [
+    Sentry.replayIntegration({
+      // Additional Replay configuration goes in here, for example:
+      maskAllText: true,
+      blockAllMedia: true,
+    }),
+    Sentry.feedbackIntegration({
+      // Additional SDK configuration goes in here, for example:
+      colorScheme: "dark",
+      isNameRequired: true,
+    }),
+  ],
+});

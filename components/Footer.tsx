c98ee7409b5e3210b0fc925bc272b54fe7e52305
T7SEN. <A.hitelare2@gmail.com>
2024-09-14 05:25:15 +0300

    Fixed an issue with the footer-grid.svg overlapping the content of the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
       {/* background grid */}
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
+      <div className="w-full absolute left-0 -bottom-72 min-h-96 z-0">
         <img
           src="/footer-grid.svg"
           alt="grid"
@@ -16,7 +16,7 @@ const Footer = () => {
         />
       </div>
 
-      <div className="flex flex-col items-center">
+      <div className="flex relative flex-col items-center z-1">
         <h1 className="heading lg:max-w-[45vw]">
           Ready to take <span className="text-purple">your</span> digital
           presence to the next level?
@@ -33,7 +33,7 @@ const Footer = () => {
           />
         </a>
       </div>
-      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+      <div className="flex relative mt-16 md:flex-row flex-col justify-between items-center z-1">
         <p className="md:text-base text-sm md:font-normal font-light">
           Made with 💜 by {" T7SEN "}
         </p>

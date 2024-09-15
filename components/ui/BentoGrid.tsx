d7e4648486446d4caae2eaf2daab4c194951de03
T7SEN. <A.hitelare2@gmail.com>
2024-09-16 02:07:49 +0300

    fixed the imports for the data folder

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -8,7 +8,7 @@ import { cn } from "@/lib/utils";
 
 import { BackgroundGradientAnimation } from "./GradientBg";
 import GridGlobe from "./GridGlobe";
-import animationData from "../data/confetti.json";
+import animationData from "../../data/confetti.json";
 import MagicButton from "../MagicButton";
 
 export const BentoGrid = ({

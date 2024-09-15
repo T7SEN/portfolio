6cce839ac221ae5f237d2814eb73c8e04aef0dc4
T7SEN. <A.hitelare2@gmail.com>
2024-09-16 01:57:10 +0300

    Fixing the imports for all the components

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import { navItems } from "@/data";
+import { navItems } from "../data";
 
-import Hero from "@/components/Hero";
-import Grid from "@/components/Grid";
-import Footer from "@/components/Footer";
-import Clients from "@/components/Clients";
-import Approach from "@/components/Approach";
-import Experience from "@/components/Experience";
-import RecentProjects from "@/components/RecentProjects";
-import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import Hero from "../components/Hero";
+import Grid from "../components/Grid";
+import Footer from "../components/Footer";
+import Clients from "../components/Clients";
+import Approach from "../components/Approach";
+import Experience from "../components/Experience";
+import RecentProjects from "../components/RecentProjects";
+import { FloatingNav } from "../components/ui/FloatingNavbar";
 
 const Home = () => {
   return (
